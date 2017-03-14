7f1714c3ca298b9a198e8348f2eb6fa1c152fae6
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-03-14 11:50:15 -0400

    Fixed typo in oracle bin setup script.

--- oracle_setup_linux.sh
+++ oracle_setup_linux.sh
@@ -14,6 +14,6 @@
 # https://www.npmjs.com/package/oracledb
 
 
-export LD_LIBRARY_PATH="/opt/oracle/instantclient:$LD_LIBRARY_PAT"
+export LD_LIBRARY_PATH="/opt/oracle/instantclient:$LD_LIBRARY_PATH"
 export OCI_LIB_DIR="/opt/oracle/instantclient"
 export OCI_INC_DIR="/opt/oracle/instantclient/sdk/include"
