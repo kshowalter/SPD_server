a62841aab8a9d3818e92c145f05a0639faa1ebfc
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-03-20 16:14:03 -0400

    Tweaked routes for testing.

--- routes.js
+++ routes.js
@@ -155,7 +155,71 @@ router.get('/t/SVG', function(req, res) {
     // update system calculations
     var system_settings = mk_settings(data);
     system_settings.server.host = req.headers.host;
-    system_settings.system_id = system_id;
+    system_settings = process_system(system_settings);
+
+    var status = system_settings.state.notes.errors.length ? 'error' : 'pass';
+
+    if( status === 'pass'){
+      // update drawing
+      system_settings = mk_drawing(system_settings);
+
+      var svgs = system_settings.drawing.svgs.map(function(svg){
+        return svg.outerHTML;
+      });
+
+      //var svg_string = svgs[sheet_num-1];
+      //if( svg_string ){
+      //  var html = html_wrap_svg(svg_string);
+      //  res.end(html);
+      //} else {
+      //  res.end();
+      //}
+
+      //mk_PDFs(system_settings, function(PDF_file_name){
+      res.json({
+        system_id: system_id,
+        status: status,
+        notes: system_settings.state.notes,
+        SVG_url: SVG_url,
+        PDF_url: PDF_url,
+        SVGs: svgs
+      });
+      //});
+
+    } else if( status === 'error' ){
+
+      res.json({
+        system_id: system_id,
+        status: status,
+        notes: system_settings.state.notes,
+        SVG_url: SVG_url,
+        PDF_url: PDF_url,
+        SVGs: []
+      });
+    }
+  });
+});
+
+
+
+///////////////////////////////////////////
+router.get('/t/SVG_page', function(req, res) {
+  var system_id = req.query.pv_system_id;
+  var sheet_num = req.query.sheet_num;
+  //var system_id = req.params.system_id;
+  var SVG_url = req.headers.host+'/d/SVG?pv_system_id='+system_id;
+  var PDF_url = req.headers.host+'/d/PDF?pv_system_id='+system_id;
+
+  var responce_string = req.method + ': ' + req.url;
+  console.log(responce_string);
+
+  //get_DB_data(req, function(data){
+  TEST_get_DB_data(req, function(data){
+    data = map_DB_data(data);
+
+    // update system calculations
+    var system_settings = mk_settings(data);
+    system_settings.server.host = req.headers.host;
     system_settings = process_system(system_settings);
 
     var status = system_settings.state.notes.errors.length ? 'error' : 'pass';
@@ -200,6 +264,8 @@ router.get('/t/SVG', function(req, res) {
     }
   });
 });
+
+
 
 
 
