55564aceab4bdb273cf12158ab5f5a423a7b375f
Keith Showalter <kshowalter@fsec.ucf.edu>
2018-03-22 14:24:31 -0400

    Added calculations to G-002.

--- drawing/page/G-002.js
+++ drawing/page/G-002.js
@@ -97,6 +97,98 @@ var mk_page = function(settings){
 
   d.layer();
 
+
+
+
+  data_to_display = state.calculation_tables;
+
+  x = x_ca;
+  y = Math.max( y_ca, y_cb );
+
+
+
+  d.layer('table');
+
+  var col_widths = [null,
+    300,
+    150,
+    450,
+    60,
+  ];
+
+  for( var section_name in data_to_display ){
+    var section = data_to_display[section_name];
+
+    var n = Object.keys(section).length;
+
+    var n_rows = section.length;
+    var n_cols = section[0].length;
+    var table_spacing = size.tables.table_spacing;
+
+    var row_height = size.tables.row_height;
+    var table_width_min = 0;
+
+    var t = d.table(n_rows,n_cols);
+    t.row_size('all', row_height);
+    t.all_cells().forEach(function(cell){
+      cell.font('table_left_calc').border('all');
+    });
+
+    for( var r = 1; r <= section.length; r++ ){
+      if(section[r-1].length === 1){
+        t.cell(r,1).text( section[r-1][0] );
+        for( var c = 1; c <= section[0].length; c++ ){
+          t.cell(r,c).font('table_note').border('L',false);
+        }
+        t.cell(r,1).border('L',true);
+        table_width_min = Math.max( table_width_min, section[r-1][0].length  * font_letter_width + text_cell_size_fixed );
+      } else {
+        for( var c = 1; c <= section[r-1].length; c++ ){
+          t.cell(r,c).text( section[r-1][c-1] );
+          //if(section[r-1][c-1].length){
+          //  col_widths[c] = col_widths[c] || 0;
+          //  col_widths[c] = Math.max(col_widths[c], section[r-1][c-1].length * font_letter_width + text_cell_size_fixed );
+          //}
+        }
+      }
+    }
+
+
+    if(col_widths.length){
+      var table_width = col_widths.reduce((a,c)=>a+c);
+    }
+    //var comment_table_ratio = table_width_min / table_width;
+    //table_width = Math.max( table_width, table_width_min );
+
+    col_widths.forEach(function(cw,i){
+      //if(comment_table_ratio>1){
+      //  cw *= comment_table_ratio;
+      //}
+      t.col_size(i, cw);
+    });
+
+    var table_height = n_rows*row_height;
+
+    t.loc(x,y);
+    d.text( [x+table_width/2, y-row_height*2/3], f.pretty_name(section_name),'table', 'table_title' );
+
+    for( var c=1; c<=n_cols; c++){
+      t.cell(1,c).font('table_col_title_left');
+    }
+    t.border_layer('border_light');
+
+    t.mk();
+
+    y += table_height + table_spacing;
+  }
+
+  d.layer();
+
+
+
+
+
+
   return d;
 };
 
