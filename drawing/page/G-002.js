8a40b840d821b9228a3ea9303be7ab7f5472c889
Keith Showalter <kshowalter@fsec.ucf.edu>
2018-03-22 13:41:03 -0400

    Rearanged G-002.

--- drawing/page/G-002.js
+++ drawing/page/G-002.js
@@ -11,10 +11,14 @@ var mk_page = function(settings){
   var size = settings.drawing_settings.size;
   var loc = settings.drawing_settings.loc;
 
-  var x = size.sheet.frame_padding + size.tables.table_spacing/2;
-
   var top = size.sheet.frame_padding + size.tables.table_spacing;
-  var y = top;
+  var x_ca = size.sheet.frame_padding + size.tables.table_spacing/2;
+  var x_cb = settings.drawing_settings.size.sheet.w/2 + size.sheet.frame_padding ;
+  var y_ca = top;
+  var y_cb = top;
+
+  var x = x_ca;
+  var y = y_ca;
 
   var text_cell_size_fixed = size.tables.text_cell_size_fixed;
   var font_letter_width = size.tables.font_letter_width;
@@ -24,7 +28,7 @@ var mk_page = function(settings){
   var col_widths = [null,
     250,
     140,
-    80,
+    90,
   ];
 
   for( var section_name in data_to_display ){
@@ -56,33 +60,18 @@ var mk_page = function(settings){
       } else {
         for( var c = 1; c <= section[r-1].length; c++ ){
           t.cell(r,c).text( section[r-1][c-1] );
-          //if(section[r-1][c-1].length){
-          //  col_widths[c] = col_widths[c] || 0;
-          //  col_widths[c] = Math.max(col_widths[c], section[r-1][c-1].length * font_letter_width + text_cell_size_fixed );
-          //}
         }
       }
     }
 
-
     if(col_widths.length){
       var table_width = col_widths.reduce((a,c)=>a+c);
     }
-    //var comment_table_ratio = table_width_min / table_width;
-    //table_width = Math.max( table_width, table_width_min );
 
     col_widths.forEach(function(cw,i){
-      //if(comment_table_ratio>1){
-      //  cw *= comment_table_ratio;
-      //}
       t.col_size(i, cw);
     });
 
-    var table_height = n_rows*row_height;
-    if( ( y+table_height+table_spacing) > ( settings.drawing_settings.size.sheet.h * 0.9 ) ) {
-      y = top;
-      x += table_width*1.1;
-    }
     t.loc(x,y);
     d.text( [x+table_width/2, y-row_height*2/3], f.pretty_name(section_name), 'table', 'table_title' );
 
@@ -93,13 +82,17 @@ var mk_page = function(settings){
 
     t.mk();
 
-    y += table_height + table_spacing;
-    //y = top;
-    //x += table_width*1.05;
-
-    if( system.config.system_type === 'optimizer' ){
-      x = size.sheet.frame_padding + size.tables.table_spacing/2;
+    var table_height = n_rows*row_height;
+    if( x === x_ca ){
+      y_ca += table_height + table_spacing;
+      x = x_cb;
+      y = y_cb;
+    } else {
+      y_cb += table_height + table_spacing;
+      x = x_ca;
+      y = y_ca;
     }
+
   }
 
   d.layer();
